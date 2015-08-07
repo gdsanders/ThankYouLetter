d026f72a39e720918f5838467d5325f049c8d26e
gdsanders <gd@digitalequity.com>
2015-08-06 19:37:41 -0500

    multiline constant test

--- ViewController.swift
+++ ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var toLabel: UILabel!
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    @IBOutlet weak var fromLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let toName = "Santa", message = "Please don't eat my damn cookies!", fromName = "Kat Daddy"
+        
+        toLabel.text = toName
+        messageLabel.text = message
+        fromLabel.text = fromName
+        
     }
 
     override func didReceiveMemoryWarning() {
